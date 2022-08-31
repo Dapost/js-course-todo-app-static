9170ba553dd86ea3208d63e9cff73486e8e36acb
Davide Postiglione <davide.postiglione@soft.it>
2022-08-31 18:43:25 +0200

    app statica

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ let tasks = [{
     {
         id: "2",
         title: "Merge branches on repository",
-        priority: false,
-        done: true,
+        priority: true,
+        done: false,
     },
     {
         id: "3",
@@ -31,6 +31,187 @@ let tasks = [{
 ]
 
 
+drawTasks();
+
+function drawTasks() {
+    const todoContainer = document.querySelector('#todo-list');
+    const doneContainer = document.querySelector('#done-list');
+
+
+    todoContainer.innerHTML = '';
+    doneContainer.innerHTML = '';
+
+    const todos = tasks.filter(task => !task.done)
+    const done = tasks.filter(task => task.done)
+
+    todos.forEach(task => {
+        const taskDom = drawTaskNode(task);
+        todoContainer.append(taskDom)
+
+        /*  const taskDom = drawTaskHTML(task);
+         todoContainer.innerHTML += taskDom; */
+    })
+
+    done.forEach(task => {
+        const taskDom = drawTaskNode(task);
+        doneContainer.append(taskDom)
+
+        /*  const taskDom = drawTaskHTML(task);
+         doneContainer.innerHTML += taskDom; */
+    })
+
+    //setUpdateEvents();
+
+}
+
+
+function drawTaskNode(task) {
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task');
+    taskContainer.id = 'task-' + task.id;
+
+    if (task.priority) {
+        const priority = document.createElement('span');
+        priority.classList.add('priority');
+        taskContainer.append(priority)
+    }
+
+    const label = document.createElement('label')
+    label.setAttribute('for', 'task-checkbox-' + task.id)
+    label.classList.add('title');
+    label.innerText = task.title;
+
+    const icon = document.createElement('i');
+    icon.classList.add('bi-pencil');
+
+    label.append(icon);
+    taskContainer.append(label);
+
+    const input = document.createElement('input');
+    input.id = "task-checkbox-" + task.id
+    input.setAttribute('type', 'checkbox')
+    input.checked = task.done;
+
+    input.addEventListener('change', () => {
+        task.done = input.checked;
+        drawTasks();
+    })
+
+    icon.addEventListener('click', (event) => {
+        event.preventDefault()
+        openModal();
+        editTask(task)
+    })
+
+    taskContainer.append(input);
+
+    return taskContainer
+
+}
+
+function drawTaskHTML(task) {
+    return `<div class="task" id="task-${task.id}">
+        ${
+            task.priority ? 
+            '<span class="priority"></span>'
+            : ''
+        }
+        <label for="task-checkbox-${task.id}"  class="title">
+        ${task.title}
+        <i class="bi-pencil" ></i>
+        </label>
+        <input id="task-checkbox-${task.id}" data-id="${task.id}" type="checkbox" ${task.done ? 'checked' : ''}>
+    </div>`
+}
+
+function setUpdateEvents() {
+    document.querySelectorAll('.task')
+        .forEach(taskDom => {
+            const icon = taskDom.querySelector('i');
+            const input = taskDom.querySelector('input')
+
+            input.addEventListener('change',
+                (event) => {
+                    const task = tasks.find(task => task.id == input.dataset.id)
+                    handleDoneCheckboxChange(task, event.target.checked)
+                }
+            )
+
+            icon.addEventListener('click', event => {
+                event.preventDefault();
+                const task = tasks.find(task => task.id == input.dataset.id)
+                openUpdateModal(task)
+            })
+        })
+}
+
+function editTask(task) {
+    const titleInput = document.querySelector('#title')
+    const priorityCheckbox = document.querySelector('#priority')
+
+    titleInput.value = task.title;
+    priorityCheckbox.checked = task.priority;
+
+    document.querySelector('#saveTaskButton')
+        .addEventListener('click', () => {
+            task.title = titleInput.value;
+            task.priority = priorityCheckbox.checked;
+            drawTasks()
+            closeModal()
+        });
+
+    const deleteModalButton = document.querySelector('#deleteModalButton')
+    deleteModalButton.style.display = 'block';
+    deleteModalButton.addEventListener('click', () => {
+        deleteTask(task)
+    })
+}
+
+function deleteTask(task) {
+    tasks = tasks.filter(element => element.id !== task.id)
+    drawTasks();
+    closeModal()
+}
+
+
+
+
+const fab = document.querySelector('.fab');
+fab.addEventListener('click', () => {
+    openModal();
+    addTask();
+})
+
+function addTask() {
+    const titleInput = document.querySelector('#title')
+    const priorityCheckbox = document.querySelector('#priority')
+
+    document.querySelector('#saveTaskButton')
+        .addEventListener('click', () => {
+            const newTask = {
+                id: Date.now(),
+                title: titleInput.value,
+                priority: priorityCheckbox.checked,
+                done: false
+            };
+
+            if (!newTask.title) {
+                alert('Inserire il titolo del task');
+                return;
+            }
+
+            tasks.push(newTask);
+            const taskDom = drawTaskNode(newTask);
+            const todoContainer = document.querySelector('#todo-list');
+            todoContainer.append(taskDom);
+
+            closeModal()
+        })
+
+
+}
+
+
 //==== MODAL
 function openModal() {
     const modal = document.querySelector('#taskModal');
@@ -51,9 +232,17 @@ function closeModal() {
     backdrop.classList.remove('open')
     modal.style.display = 'none';
 
-    //Rimuove tutti i listeners
-    const saveModalButton = document.querySelector('#saveTaskButton');
-    saveModalButton.replaceWith(saveModalButton.cloneNode(true));
+    const titleInput = document.querySelector('#title')
+    const priorityCheckbox = document.querySelector('#priority')
+
+    titleInput.value = '';
+    priorityCheckbox.checked = false;
+
+    const saveModalButton = document.querySelector('#saveTaskButton')
+    saveModalButton.replaceWith(saveModalButton.cloneNode(true))
+
+    document.querySelector('#deleteModalButton')
+        .style.display = 'none'
 
     const deleteModalButton = document.querySelector('#deleteModalButton');
     deleteModalButton.replaceWith(deleteModalButton.cloneNode(true))
